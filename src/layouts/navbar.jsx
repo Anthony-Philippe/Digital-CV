f7ea07431bb3131122dfa0960376fff863c9bfac
DindonDesAlpes <anthony.philippe@isen.yncrea.fr>
2023-08-27 21:18:12 +0200

    adding navbar components

--- src/layouts/navbar.jsx
+++ src/layouts/navbar.jsx
@@ -1,14 +1,22 @@
 export default function Navbar() {
+    const buttonsData = [
+        { title: 'Test 1', link: '/lien-vers-test1' },
+        { title: 'Test 2', link: '/lien-vers-test2' },
+        { title: 'Test 3', link: '/lien-vers-test3' },
+    ];
+
     return (
-        <>
-            <nav>
-                <img />
-                <ul>
-                    <li><button></button></li>
-                    <li><button></button></li>
-                    <li><button></button></li>
-                </ul>
-            </nav>
-        </>
+        <nav>
+            <img src="" alt=""/>
+            <ul>
+                {buttonsData.map((button, index) => (
+                    <li key={index}>
+                        <button>
+                            <a href={button.link}>{button.title}</a>
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     );
 };
