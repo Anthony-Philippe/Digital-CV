e0ad42a9d071aa2e412c02f366a3fa1940420651
DindonDesAlpes <anthony.philippe@isen.yncrea.fr>
2023-08-29 00:11:59 +0200

    Adding error (page 404 system)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './sass/app.scss'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/'
 import Cv from './pages/cv/'
+import Error from './components/error'
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/cv" element={<Cv />} />
+                <Route path="*" element={<Error />} />
             </Routes>
         </Router>
     );
