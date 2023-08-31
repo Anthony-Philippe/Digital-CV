c9f23c7dfea16ce905c058ff6f27620946621032
DindonDesAlpes <anthony.philippe@isen.yncrea.fr>
2023-08-31 21:08:44 +0200

    adding infinite caroussel

---
+++ src/components/infinite_caroussel.jsx
@@ -0,0 +1,37 @@
+import Tooltip from './tools/toolTip'
+
+const altStyles = {
+  'VHDL': { transform: 'translateY(5px)' },
+  'Packet Tracer': { transform: 'translateY(-10px)' },
+  'KiCad': { transform: 'translateY(5px)' },
+  'Nmap': { transform: 'translateY(-15px)' },
+};
+
+const renderLogosWithTooltips = (logos) => (
+  logos.map((logo, index) => {
+    const imageStyle = altStyles[logo.alt] ?? {};
+    return (
+      <Tooltip key={index} text={logo.alt}>
+        <img src={`/icons/${logo.src}`} alt={logo.alt} style={imageStyle} />
+      </Tooltip>
+    );
+  })
+);
+
+export default function infinite_Caroussel({ iconSource, speed}) {
+  const calculatedSpeed = speed ?? iconSource.length / 0.5;
+
+  const sections = Array.from({ length: 3 }, (_, index) => (
+    <section key={index} style={{ "--speed": `${calculatedSpeed}s` }}>
+      {renderLogosWithTooltips(iconSource)}
+    </section>
+  ));
+    
+  return (
+    <div className="slider_icon">
+      <div className="wrapper">
+        {sections}
+      </div>
+    </div>
+  );
+}
--- src/pages/cv.jsx
+++ src/pages/cv.jsx
@@ -2,13 +2,15 @@ import Navbar from '../layouts/navbar';
 import Footer from '../layouts/footer';
 
 import Timeline from '../components/timeline';
-import { formation_Data } from '../data/cv'
+import Caroussel from '../components/infinite_caroussel';
+import { formation_Data, codingIcon_Data } from '../data/cv'
 
 export default function Home() {
     return (
         <>
             <Navbar />
             <Timeline Datas={formation_Data} Part="Formations" circleContent="1"/>
+            <Caroussel iconSource={codingIcon_Data} />
             <Footer />
         </>
     );
